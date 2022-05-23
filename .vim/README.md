6f8f3d634f78b002ac37a5f807f28aa3b070422f
Mogeko <zhengjunyi@live.com>
2022-05-23 04:09:18 +0200

    refactor: built-in plug.vim support

---
+++ .vim/README.md
@@ -0,0 +1,24 @@
+# .vimrc
+
+Fork from [mogeko/.vimrc (gist)](https://gist.github.com/mogeko/291effaf70cce916ce8b1c2a964fcc0d).
+
+Built-in [plug.vim](https://github.com/junegunn/vim-plug) support.
+
+## Install
+
+Open the terminal and run:
+
+```
+curl -fL -o ~/.vimrc -o ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/mogeko/dotfiles/master/{.vimrc,.vim/autoload/plug.vim}
+```
+
+For Archlinux:
+
+```
+sudo pacman -Syu vim-plug
+
+curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/mogeko/dotfiles/master/.vimrc
+```
+
+Then open vim and execute `:PlugInstall`.
--- setup.sh
+++ setup.sh
@@ -10,33 +10,19 @@ fi
 
 echo -e "\nThe current operating system is: ${_OS_NAME}.\n"
 
-# Install Homebrew
+# Install dotfiles
+echo "Installing dotfiles..."
 if [ "${_OS_NAME}" == "darwin" ]; then
+  install -v -m 0644 "./bash/macos.bash_profile" "${HOME}/.bash_profile"
+  install -v -m 0644 "./bash/macos.bash_aliases" "${HOME}/.bash_aliases"
+  # Install Homebrew
   if [ -f "/opt/homebrew/bin/brew" ]; then
     echo -e "\nHomebrew is already installed.\n"
   else
     echo -e "\nInstalling Homebrew...\n"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
-fi
-
-# Download plug.vim
-echo "Downloading plug.vim to ./tmp/plug.vim ..."
-echo "See https://github.com/junegunn/vim-plug."
-curl -fLo "./tmp/plug.vim" --create-dirs \
-  "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-echo -e "Done.\n"
-
-# Install dotfiles
-echo "Installing dotfiles..."
-if [ "${_OS_NAME}" == "darwin" ]; then
-  install -v -m 0644 "./bash/macos.bash_profile" "${HOME}/.bash_profile"
-  install -v -m 0644 "./bash/macos.bash_aliases" "${HOME}/.bash_aliases"
 elif [ "${_OS_NAME}" == "linux" ]; then
   install -v -m 0644 "./bash/linux.bashrc" "${HOME}/.bashrc"
   install -v -m 0644 "./bash/linux.bash_aliases" "${HOME}/.bash_aliases"
 fi
-
-install -v -m 0644 "./git/.gitconfig" "${HOME}/.gitconfig"
-install -v -m 0644 "./tmp/plug.vim" "${HOME}/.vim/autoload/plug.vim"
-install -v -m 0644 "./vim/.vimrc" "${HOME}/.vimrc"
